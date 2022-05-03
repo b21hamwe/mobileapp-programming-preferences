cfe69f1c33d4e3b91992c874ea0ab43c2ba3f3f8
Hampus Wetterlind <b21hamwe@student.his.se>
2022-05-03 14:39:33 +0200

    Created layout with button and edit text + skapat namnet Hampus i preferences i MainActivity filen

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.project;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textviewname;
+    SharedPreferences preferences;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +19,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        textviewname = findViewById(R.id.name);
+
+
+
+        preferences = getSharedPreferences("preferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("name","Hampus");
+        editor.apply();
+
+
     }
 
+
+
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+        String name = preferences.getString("name","inget namn hittades");
+        textviewname.setText(name);
+    }
 }
