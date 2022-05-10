2800c219169a7d9962e683f60c42de2caf56b1c5
Hampus Wetterlind <b21hamwe@student.his.se>
2022-05-10 17:13:49 +0200

    Created member variables to execute onClicklistener to shut down and save strings by buttons

--- app/src/main/java/com/example/project/WriteActivity.java
+++ app/src/main/java/com/example/project/WriteActivity.java
@@ -2,6 +2,9 @@ package com.example.project;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,23 +17,50 @@ public class WriteActivity extends AppCompatActivity {
 
     TextView textviewname;
     SharedPreferences preferences;
+    Button close;
+    Button save;
+    EditText text;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         textviewname = findViewById(R.id.spara);
-
-
-
+        close = findViewById(R.id.CLOSE);
+        save = findViewById(R.id. spara);
+        text = findViewById(R.id.edit_text);
         preferences = getSharedPreferences("preferences", MODE_PRIVATE);
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putString("name","Hampus");
-        editor.apply();
+
+
+
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String input = text.getText().toString();
+
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString("name",input);
+                editor.apply();
+            }
+        });
+
+
+
+
 
 
     }
