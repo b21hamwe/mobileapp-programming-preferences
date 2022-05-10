ae0023cf976644599aab66237d131bf07d014f91
Hampus Wetterlind <b21hamwe@student.his.se>
2022-05-10 17:15:03 +0200

    Created member variables and onClickListener to open the WriteActivity file

--- app/src/main/java/com/example/project/ReadActivity.java
+++ app/src/main/java/com/example/project/ReadActivity.java
@@ -2,8 +2,11 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ReadActivity extends AppCompatActivity {
@@ -11,13 +14,25 @@ public class ReadActivity extends AppCompatActivity {
 
     TextView textviewname;
     SharedPreferences preferences;
+    Button open;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-    }
+        setContentView(R.layout.activity_main);
 
+        textviewname = findViewById(R.id.viewpage2);
+        preferences = getSharedPreferences("preferences", MODE_PRIVATE);
+        open = findViewById(R.id.open);
+
+        open.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ReadActivity.this, WriteActivity.class));
+            }
+        });
+
+    }
 
     @Override
     protected void onResume() {
